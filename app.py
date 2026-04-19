f5b0244064f91c3266a05023a28ba0b3386737f3
uacc77552-cmd <uacc77552@gmail.com>
2026-04-19 11:15:28 +0600

    First version of Mobile price classification

---
+++ app.py
@@ -0,0 +1,55 @@
+import numpy as np
+import gradio as gr
+
+def predict_price(battery_power, blue, clock_speed, dual_sim, fc, four_g,
+                  int_memory, m_dep, mobile_wt, n_cores, pc,
+                  px_height, px_width, ram, sc_h, sc_w,
+                  talk_time, three_g, touch_screen, wifi):
+
+    
+    features = np.array([[battery_power, blue, clock_speed, dual_sim, fc, four_g,
+                          int_memory, m_dep, mobile_wt, n_cores, pc,
+                          px_height, px_width, ram, sc_h, sc_w,
+                          talk_time, three_g, touch_screen, wifi]])
+    
+    prediction = final_model.predict(features)[0]
+    
+    labels = {
+        0: "Low Cost",
+        1: "Medium Cost",
+        2: "High Cost",
+        3: "Very High Cost"
+    }
+    
+    return labels[prediction]
+
+interface = gr.Interface(
+    fn=predict_price,
+    inputs=[
+        gr.Number(label="Battery Power"),
+        gr.Radio([0,1], label="Bluetooth (0=No, 1=Yes)"),
+        gr.Number(label="Clock Speed"),
+        gr.Radio([0,1], label="Dual SIM"),
+        gr.Number(label="Front Camera (MP)"),
+        gr.Radio([0,1], label="4G"),
+        gr.Number(label="Internal Memory"),
+        gr.Number(label="Mobile Depth"),
+        gr.Number(label="Weight"),
+        gr.Number(label="Number of Cores"),
+        gr.Number(label="Primary Camera"),
+        gr.Number(label="Pixel Height"),
+        gr.Number(label="Pixel Width"),
+        gr.Number(label="RAM"),
+        gr.Number(label="Screen Height"),
+        gr.Number(label="Screen Width"),
+        gr.Number(label="Talk Time"),
+        gr.Radio([0,1], label="3G"),
+        gr.Radio([0,1], label="Touch Screen"),
+        gr.Radio([0,1], label="WiFi")
+    ],
+    outputs=gr.Textbox(label="Predicted Price Range"),
+    title=" Mobile Price Classification ",
+    description="predict price category"
+)
+
+interface.launch(share=True)
---
+++ rf_train.py
@@ -0,0 +1,140 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#sklearn preprocessing
+
+from sklearn.model_selection import train_test_split,cross_val_score,GridSearchCV
+from sklearn.preprocessing import StandardScaler,OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+
+
+#Regression model
+
+from sklearn.linear_model import LinearRegression,Ridge
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor,RandomForestClassifier
+
+
+
+from sklearn.ensemble import VotingRegressor, StackingRegressor
+
+
+#metrices
+
+from sklearn.metrics import mean_squared_error,r2_score,mean_absolute_error,accuracy_score,classification_report, confusion_matrix
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+df = pd.read_csv("/content/train.csv")
+df
+df.head()
+df.shape
+
+df.isnull().sum()
+df.fillna(df.median(), inplace=True)
+
+binary_cols = ['blue','dual_sim','four_g','three_g','touch_screen','wifi']
+
+for col in binary_cols:
+    df[col] = df[col].astype(int)
+
+
+scaler = StandardScaler()
+
+features = df.drop('price_range', axis=1)
+target = df['price_range']
+
+scaled_features = scaler.fit_transform(features)
+
+df_scaled = pd.DataFrame(scaled_features, columns=features.columns)
+df_scaled['price_range'] = target.values
+
+Q1 = df.quantile(0.25)
+Q3 = df.quantile(0.75)
+
+IQR = Q3 - Q1
+
+df = df[~((df < (Q1 - 1.5 * IQR)) | (df > (Q3 + 1.5 * IQR))).any(axis=1)]
+
+df['screen_area'] = df['px_height'] * df['px_width']
+df['battery_efficiency'] = df['battery_power'] / (df['ram'] + 1)
+
+X = df.drop(['price_range'], axis=1)
+y = df['price_range']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+pipeline = Pipeline([
+    ('imputer', SimpleImputer(strategy='median')),
+    ('scaler', StandardScaler()),
+    ('model', RandomForestClassifier(
+        n_estimators=100,
+        random_state=42
+    ))
+])
+pipeline.fit(X_train, y_train)
+
+model = RandomForestClassifier(
+    n_estimators=100,
+    random_state=42
+)
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+print("Training Accuracy:", model.score(X_train, y_train))
+print("Test Accuracy:", accuracy_score(y_test, y_pred))
+
+scores = cross_val_score(model, X, y, cv=5, scoring='accuracy')
+print("Cross-Validation Scores:", scores)
+print("Average Accuracy:", np.mean(scores))
+print("Standard Deviation:", np.std(scores))
+
+rf = RandomForestClassifier(random_state=42)
+param_grid = {
+    'n_estimators': [50, 100, 200],
+    'max_depth': [10, 20, None],
+    'min_samples_split': [2, 5, 10],
+    'max_features': ['sqrt', 'log2']
+}
+grid_search = GridSearchCV(
+    estimator=rf,
+    param_grid=param_grid,
+    cv=5,
+    scoring='accuracy',
+    n_jobs=-1
+)
+
+grid_search.fit(X, y)
+
+print("Best Parameters Found:")
+print(grid_search.best_params_)
+
+print("\nBest Cross-Validation Accuracy:")
+print(grid_search.best_score_)
+
+best_params = grid_search.best_params_
+
+final_model = RandomForestClassifier(
+    n_estimators=best_params['n_estimators'],
+    max_depth=best_params['max_depth'],
+    min_samples_split=best_params['min_samples_split'],
+    max_features=best_params['max_features'],
+    random_state=42
+)
+final_model.fit(X, y)
+
+y_pred = final_model.predict(X_test)
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Classification Report:\n")
+print(classification_report(y_test, y_pred))
+print("Confusion Matrix:\n")
+print(confusion_matrix(y_test, y_pred))
+
